4894b803552a24cdd4dfaf90989f2740cd6627cb
Varun Sharma <67771380+Varun2323@users.noreply.github.com>
2021-01-20 22:46:58 +0530

    linters errors corrected

--- lib/checker.rb
+++ lib/checker.rb
@@ -62,41 +62,41 @@ class CheckerError
   def check_end_empty_line(value, index)
     return unless value.strip.split(' ').first.eql?('end')
 
-    if @checker.file_content[index - 1].strip.empty?
-      log_error("line:#{index} Extra Empty line detected at block body end")
+    return unless @checker.file_content[index - 1].strip.empty?
+
+    log_error("line:#{index} Extra Empty line detected at block body end")
+  end
+end
+
+def check_do_empty_line(value, index)
+  message = 'Extra empty line detetced at block body beginning'
+  return unless value.strip.split(' ').include?('do')
+
+  log_error("line:#{index + 2} #{message}") if @checker.file_content[index + 1].strip.empty?
+end
+
+def log_error(error_msg)
+  @errors << error_msg
+end
+
+def check_white_spaces
+  @checker.file_content.each_with_index do |value, index|
+    if value[-2] == ' ' && !value.strip.empty?
+      @errors << "line:#{index + 1}:#{value.size - 1}: Error: whitespace detected"
+      + " '#{value.gsub(/\s*$/, '_')}'"
     end
   end
+end
 
-  def check_do_empty_line(value, index)
-    message = 'Extra empty line detetced at block body beginning'
-    return unless value.strip.split(' ').include?('do')
-
-    log_error("line:#{index + 2} #{message}") if @checker.file_content[index + 1].strip.empty?
+def end_error
+  start_position = 0
+  last_position = 0
+  @checker.file_content.each_with_index do |value, _index|
+    start_position += 1 if @keywords.include?(value.split(' ').first) || value.split(' ').include?('do')
+    last_position += 1 if value.strip == 'end'
   end
 
-  def log_error(error_msg)
-    @errors << error_msg
-  end
-
-  def check_white_spaces
-    @checker.file_content.each_with_index do |value, index|
-      if value[-2] == ' ' && !value.strip.empty?
-        @errors << "line:#{index + 1}:#{value.size - 1}: Error: whitespace detected"
-        + " '#{value.gsub(/\s*$/, '_')}'"
-      end
-    end
-  end
-
-  def end_error
-    start_position = 0
-    last_position = 0
-    @checker.file_content.each_with_index do |value, _index|
-      start_position += 1 if @keywords.include?(value.split(' ').first) || value.split(' ').include?('do')
-      last_position += 1 if value.strip == 'end'
-    end
-
-    status_check = start_position <=> last_position
-    log_error("Lint/Syntax: Missing 'end'") if status_check.eql?(1)
-    log_error("Lint/Syntax: Unexpected 'end'") if status_check.eql?(-1)
-  end
+  status_check = start_position <=> last_position
+  log_error("Lint/Syntax: Missing 'end'") if status_check.eql?(1)
+  log_error("Lint/Syntax: Unexpected 'end'") if status_check.eql?(-1)
 end
